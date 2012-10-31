98a6fdbccc922bd973865a86c53d24cbbd8ed5ff
Jakub Mrozek <jakub.mrozek@gmail.com>
2012-10-31 01:02:47 +0100

    Vlozeni stranky s nazvem new znemozni vlozeni dalsi nove stranky

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ angular.module('zdrojak', ['zdrojakServices'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/', {templateUrl: '/partials/index/index.html', controller: IndexIndexCtrl});
         $routeProvider.when('/pages', {templateUrl: '/partials/pages/index.html', controller: PagesIndexCtrl});
-        $routeProvider.when('/pages/new', {templateUrl: '/partials/pages/new.html', controller: PagesNewCtrl});
+        $routeProvider.when('/pages/new/page', {templateUrl: '/partials/pages/new.html', controller: PagesNewCtrl});
         $routeProvider.when('/pages/:page', {templateUrl: '/partials/pages/show.html', controller: PagesShowCtrl});
         $routeProvider.when('/pages/:page/edit', {templateUrl: '/partials/pages/edit.html', controller: PagesEditCtrl});
         $routeProvider.otherwise({redirectTo: '/'});
