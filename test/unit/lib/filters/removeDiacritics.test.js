a75ef43d2b53130caa9c6e82863af17c852da95b
Jiri Prokop <jiri-prokop@synaptiko.cz>
2012-11-05 19:21:30 +0100

    Zapomenute soubory: Implementace removeDiacritics filtru a jeho pridani do url filtru; Vylepseni samotneho url filtru a pridani dalsich testu.

---
+++ test/unit/lib/filters/removeDiacritics.test.js
@@ -0,0 +1,9 @@
+var url = require(process.cwd() + '/lib/filters/removeDiacritics');
+
+describe('remove diacritics filter', function(){
+    it('odstrani diakritiku', function(){
+        url('příliš žluťoučký kůň úpěl ďábelské ódy').should.eql('prilis zlutoucky kun upel dabelske ody');
+        url('PŘÍLIŠ ŽLUŤOUČKÝ KŮŇ ÚPĚL ĎÁBELSKÉ ÓDY').should.eql('PRILIS ZLUTOUCKY KUN UPEL DABELSKE ODY');
+    })
+});
+
