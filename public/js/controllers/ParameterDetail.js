13df10aacdf347d5e83432ce687b2a33f181a39e
Jakub Mrozek <jakub.mrozek@gmail.com>
2013-08-03 15:59:19 +0200

    Dokonceni sekce Parametry

--- public/js/controllers/ParameterDetail.js
+++ public/js/controllers/ParameterDetail.js
@@ -1,15 +1,35 @@
 angular.module('zdrojak.controller').controller('ParameterDetailCtrl', ['$scope', '$location', '$routeParams', 'api', function ($scope, $location, $routeParams, api) {
-  $scope.parameter = api.parameter.show({id: $routeParams.id});
-  $scope.options = [
-    {name: 'Číselník', value: 'codebook'},
-    {name: 'Hodnota', value: 'value'}
-  ];
+  $scope.codebook = [];
+
+  $scope.parameter = api.parameter.show({id: $routeParams.id}, function(){
+  	var codebook = $scope.parameter.values;
+  	for (var i = 0; i < codebook.length; ++i) $scope.codebook.push({value: codebook[i]});
+    addValue();
+  });
+
   $scope.options = {
   	codebook: 'Číselník',
   	value: 'Hodnota'
   };
 
+  var addValue = function() {
+    $scope.codebook.push({value: ''});
+  }; 
+
+  $scope.check = function() {
+    var last = $scope.codebook[$scope.codebook.length - 1];
+    if (last.value !== '') addValue();
+  };
+
   $scope.update = function() {
-    console.log('ok');
+    var data = { name: $scope.name, type: $scope.type, values: $scope.codebook };
+  	api.parameter.update({id: $routeParams.id}, data);
+  };
+
+  $scope.remove = function() {
+    if (!window.confirm('Chcete skutečně parametr smazat?')) return;
+  	api.parameter.remove({id: $routeParams.id}, function(){
+      $location.url('/admin/parameters');
+    });
   };
 }]);
