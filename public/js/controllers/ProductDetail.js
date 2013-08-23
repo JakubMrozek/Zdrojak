b6891e75bc10e3fd689165e8107dc2a3bb1bfbf6
Jakub Mrozek <jakub.mrozek@gmail.com>
2013-08-24 00:00:23 +0200

    Detail produktu

--- public/js/controllers/ProductDetail.js
+++ public/js/controllers/ProductDetail.js
@@ -1,8 +1,48 @@
-angular.module('zdrojak.controller').controller('ProductDetailCtrl', ['$scope', '$routeParams', '$location', '$window', 'api', 'uploadFile', function($scope, $routeParams, $location, $window, api, uploadFile) {
-  
+angular.module('zdrojak.controller').controller('ProductDetailCtrl', ['$scope', '$routeParams', '$location', '$window', '$q', 'api', 'uploadFile', function($scope, $routeParams, $location, $window, $q, api, uploadFile) {
+
+  var productLoaded = false;
+  var paramsLoaded = false;
+
+  var addParamsToProduct = function() {
+    if (!productLoaded || !paramsLoaded) return;
+    $scope.product.parameters.forEach(function(param, key){
+      for (var i = 0; i < $scope.parameters.length; ++i) {
+        var parameter = $scope.parameters[i];
+        if (parameter.id == param.id) {
+          $scope.product.parameters[key].type = parameter.type;
+          if (parameter.type == 'codebook') {
+            var checkedVals = $scope.product.parameters[key].values;
+            var values = {};
+            for (var v in parameter.values) {
+              values[parameter.values[v]] = false;
+              checkedVals.forEach(function(val){
+                if (parameter.values[v].toLowerCase() == val.toLowerCase()) {
+                  values[parameter.values[v]] = true;
+                }
+              });
+            }
+            $scope.product.parameters[key].values = values;
+          }
+        }
+      }
+    });
+  };
+
+  $scope.select = function() {
+    console.log(arguments);
+  }
+
   $scope.imgs = [];
   $scope.progress = 0;
-  $scope.product = api.product.show({id: $routeParams.id});
+  $scope.product = api.product.show({id: $routeParams.id}, function(){
+    productLoaded = true;
+    addParamsToProduct();
+  });
+  $scope.parameters = api.parameter.index(function(){
+    paramsLoaded = true;
+    addParamsToProduct();
+  });
+
 
   var error = function() {
     $scope.$apply(function(){
